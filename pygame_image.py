c14b1e1f87ec392486c06fa42a8d3701e508c4ba
c0a22021 <c0a22021b6@edu.teu.ac.jp>
2023-11-21 15:40:32 +0900

    練習3

--- pygame_image.py
+++ pygame_image.py
@@ -9,6 +9,7 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg") #練習1 背景画像Surfaceの生成
     kk_img = pg.image.load("ex01/fig/3.png")# 練習2 こうかとん画像Surfaceの生成
     kk_img = pg.transform.flip(kk_img, True, False) #練習2　こうかとんの左右反転
+    kk_imgs = [kk_img, pg.transform.rotozoom[kk_img, 10, 1.0]] #練習3 回転こうかとんSurfaceのリスト
 
     tmr = 0
     while True:
@@ -16,7 +17,7 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0]) #練習4  背景画像の表示
-        screen.blit(kk_img, [300, 200])
+        screen.blit(kk_imgs[1], [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
